97f160e43b5b592ac6b5ac26921c610ff34ea72b
Mohammad Kashem <mohammad.kashem@outlook.co.nz>
2018-12-25 19:20:14 +1300

    starting.

---
+++ websocket_server.cpp
@@ -0,0 +1,10 @@
+#include "websocket_server.h"
+
+websocket_server::websocket_server() : websocket_helper()
+{
+}
+
+
+websocket_server::~websocket_server()
+{
+}
---
+++ websocket_server.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "websocket_helper.h"
+class websocket_server : public websocket_helper
+{
+public:
+	websocket_server();
+	virtual ~websocket_server();
+};
+
